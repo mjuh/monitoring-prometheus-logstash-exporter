7f550f19f2e4ac6fbe74dad175184039b4179d39
Oleg Pykhalov <go.wigust@gmail.com>
2022-07-21 20:30:04 +0300

    flake: Add container.

--- flake.nix
+++ flake.nix
@@ -2,10 +2,13 @@
   description = "logstash prometheus exporter";
 
   outputs = { self, nixpkgs, ... }:
-    let system = "x86_64-linux";
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      inherit (pkgs) callPackage;
     in {
-      packages.${system} = {
-        default = nixpkgs.legacyPackages.${system}.callPackage
+      packages.${system} = rec {
+        default = callPackage
           ({ lib, go, buildGoModule }:
             buildGoModule rec {
               pname = "logstash-exporter";
@@ -35,6 +38,66 @@
                 platforms = platforms.unix;
               };
             }) { };
+
+        container = callPackage
+          ({ dockerTools
+           , locale
+           , tzdata
+           , prometheus-logstash-exporter }:
+             dockerTools.buildLayeredImage {
+               name = "docker-registry.intr/monitoring/prometheus-logstash-exporter";
+               tag = "master";
+               contents = [
+                 locale
+                 tzdata
+                 prometheus-logstash-exporter
+               ];
+               config = {
+                 Env = [
+                   "TZDIR=${tzdata}/share/zoneinfo"
+                   "LOCALE_ARCHIVE=${locale}/lib/locale/locale-archive"
+                 ];
+                 Entrypoint = [ "/bin/prometheus-logstash-exporter" ];
+               };
+             })
+          {
+            prometheus-logstash-exporter = default;
+          };
+      };
+
+      apps.${system} = {
+        container-structure-test = let
+          inherit (pkgs) runtimeShell writeScriptBin;
+          config = let
+            inherit (builtins) toJSON;
+            inherit (pkgs) writeText;
+          in writeText "container-structure-test.yaml" (toJSON {
+            fileExistenceTests = [
+              {
+                name = "root";
+                path = "/";
+                shouldExist = true;
+              }
+              {
+                name = "prometheus-logstash-exporter";
+                path = "/bin/prometheus-logstash-exporter";
+                shouldExist = true;
+              }
+            ];
+            schemaVersion = "2.0.0";
+          });
+          script = writeScriptBin "container-structure-test" ''
+            #!${runtimeShell} -e
+            docker run --volume /var/run/docker.sock:/var/run/docker.sock \
+                       --volume ${config}:/container-structure-test.yaml \
+                       gcr.io/gcp-runtimes/container-structure-test:latest \
+                       test --image docker-registry.intr/monitoring/prometheus-logstash-exporter:master \
+                            --config container-structure-test.yaml
+         '';
+        in {
+          type = "app";
+          program = "${script}/bin/container-structure-test";
+        };
       };
     };
 }
